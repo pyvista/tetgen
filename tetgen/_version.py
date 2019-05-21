dc4b3d579860aaa665ce8e005d79ffd2fb2c03f6
Alex Kaszynski <akascap@gmail.com>
2019-05-21 19:43:35 +0200

    appveyor build fix

--- tetgen/_version.py
+++ tetgen/_version.py
@@ -1,7 +1,7 @@
 """ tetgen version """
 
 # major, minor, patch, -extra
-version_info = 0, 3, 2
+version_info = 0, 3, 3
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
