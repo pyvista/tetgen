4a5802c9dd97ca259fb306e55d7435d5dfc2b037
Alex Kaszynski <akascap@gmail.com>
2020-07-01 23:25:25 +0200

    fixed setup to account for a variety of build scenarios

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 """Setup for tetgen"""
 import os
 import sys
-
+import builtins
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 from io import open as io_open
 
-try:
-    import numpy
-except:
-    raise ImportError('Please install numpy first')
-
-if sys.version_info[0] < 3:
-    import __builtin__ as builtins
-else:
-    import builtins
 
 # Version from file
 __version__ = None
@@ -23,6 +14,30 @@ with io_open(version_file, mode='r') as fd:
     exec(fd.read())
 
 
+def needs_cython():
+    """Check if cython source exist"""
+    tgt_path = os.path.join('tetgen', 'cython', 'tetgen')
+    has_cython_src = any(['_tetgen.cxx' in fname for fname in os.listdir(tgt_path)])
+    if not has_cython_src:
+        try:
+            import cython
+        except ImportError:
+            raise ImportError('Please install cython to build ``tetgen``')
+    return not has_cython_src
+
+
+def needs_numpy():
+    """Check if cython source exist"""
+    tgt_path = os.path.join('tetgen')
+    has_cython_src = any(['_tetgen' in fname for fname in os.listdir(tgt_path)])
+    return not has_cython_src
+
+
+setup_requires = []
+if needs_cython():
+    setup_requires.extend(['cython'])
+if needs_numpy():
+    setup_requires.extend(['numpy'])
 
 # for: the cc1plus: warning: command line option '-Wstrict-prototypes'
 class build_ext(_build_ext):
@@ -43,8 +58,8 @@ class build_ext(_build_ext):
             pass
         _build_ext.build_extensions(self)
 
+
 # compiler args
-macros = []
 if os.name == 'nt':  # windows
     extra_compile_args = ['/openmp', '/O2', '/w', '/GS']
 elif os.name == 'posix':  # linux org mac os
@@ -55,39 +70,38 @@ else:
 
 setup(
     name='tetgen',
-    packages = ['tetgen'],
+    packages=['tetgen'],
     version=__version__,
     description='Python interface to tetgen',
     long_description=open('README.rst').read(),
 
     author='Alex Kaszynski',
     author_email='akascap@gmail.com',
-    url = 'https://github.com/pyvista/tetgen',
+    url='https://github.com/pyvista/tetgen',
 
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # Build cython modules
     cmdclass={'build_ext': build_ext},
-    ext_modules = [Extension("tetgen._tetgen", 
-                             ['tetgen/cython/tetgen/_tetgen.pyx', 
-                              'tetgen/cython/tetgen/tetgen.cxx', 
-                              'tetgen/cython/tetgen/predicates.cxx',
-                              'tetgen/cython/tetgen/tetgen_wrap.cxx'],
-                             language='c++',
-                             extra_compile_args=extra_compile_args,
-                             define_macros=[('TETLIBRARY', None)]),
+    ext_modules=[Extension("tetgen._tetgen",
+                           ['tetgen/cython/tetgen/_tetgen.pyx',
+                            'tetgen/cython/tetgen/tetgen.cxx',
+                            'tetgen/cython/tetgen/predicates.cxx',
+                            'tetgen/cython/tetgen/tetgen_wrap.cxx'],
+                           language='c++',
+                           extra_compile_args=extra_compile_args,
+                           define_macros=[('TETLIBRARY', None)]),
                    ],
-
     keywords='TetGen',
-
     install_requires=['numpy>1.9.3',
-                      'pyvista>=0.19.0']
+                      'pyvista>=0.23.0'],
+    setup_requires=setup_requires,
 )
