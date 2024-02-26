424ecc51de6b1257d9ec65a61305d781becd4a5b
Alex Kaszynski <akascap@gmail.com>
2024-02-25 22:34:02 -0700

    bump to v0.6.4

--- tetgen/_version.py
+++ tetgen/_version.py
@@ -8,7 +8,7 @@ version_info = 0, 27, 'dev0'
 """
 
 # major, minor, patch, -extra
-version_info = 0, 6, 3
+version_info = 0, 6, 4
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
