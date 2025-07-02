7c77f65d09f123a602583de750cd973c46415e4b
thefloe <tfloery@gmail.com>
2025-07-02 07:24:22 +0200

    move current region test to examples and create a new test script

---
+++ examples/regions.py
@@ -0,0 +1,73 @@
+# Example of using TetGen to create a tetrahedral mesh with regions
+
+import pyvista as pv
+import tetgen
+import numpy as np
+
+# we create a box of air with two spheres inside it
+r = 0.1 # 100mm, radius of the spheres
+L = 0.5 # 500mm, distance of centers
+# this would give a capacitance of around 6.99pF between the two spheres
+
+# create geometries
+airbox = pv.Cube(center=[0, 0, 0], x_length=3*L, y_length=L, z_length=L).triangulate()
+sphere1 = pv.Sphere(theta_resolution=16, phi_resolution=16, center=[-L/2,0,0], radius=r)
+sphere2 = pv.Sphere(theta_resolution=16, phi_resolution=16, center=[L/2,0,0], radius=r)
+
+# merge the geometries
+mesh = pv.merge([sphere1,sphere2, airbox])
+
+# create a plotter
+plotter = pv.Plotter(shape=(1, 2))
+
+# plot the input mesh
+plotter.add_mesh(mesh, show_edges=True, opacity=0.5, label='Input Mesh')
+plotter.add_text("Input Mesh", font_size=10)
+
+# create a subplot for the tetrahedralized mesh
+plotter.subplot(0, 1)
+plotter.add_text("Tetrahedralized Mesh", font_size=10)
+
+# tetrahedralize the mesh using TetGen
+tgen = tetgen.TetGen(mesh)
+
+# add regions to the TetGen object
+V_sphere = 4/3 * np.pi * r**3
+tgen.addRegion(100, [-L/2, 0, 0], V_sphere / 5000)    # sphere 1 
+tgen.addRegion(200, [L/2, 0, 0], V_sphere / 5000)     # sphere 2    
+tgen.addRegion(300, [L*1.5, 0, 0], V_sphere / 100)    # airbox
+
+# tetrahedralize the mesh with specified switches
+nodes, elem, attrib = tgen.tetrahedralize(switches="pzq1.4Aa")
+
+grid = tgen.grid
+
+# retrieve the region IDs from the attributes
+regions = np.unique(attrib[:, 0])
+    
+# plot regions with different colors and opacities
+for reg_id in regions:
+    name = f'Region {reg_id}'
+    reg_mask = attrib[:, 0] == reg_id
+    subgrid = grid.extract_cells(reg_mask)
+        
+    if reg_id == 100:
+        color = 'red'
+    elif reg_id == 200:
+        color = 'blue'
+    else:
+        color = 'white'
+        
+    if reg_id < 300:
+        opacity = 1.0
+    else:
+        opacity = 0.25
+        
+    plotter.add_mesh(subgrid, show_edges=True, color=color, opacity=opacity, label=name)
+
+
+plotter.add_legend()  
+plotter.link_views()
+
+# finally show the results
+plotter.show()
--- tests/test_regions.py
+++ tests/test_regions.py
@@ -1,92 +1,31 @@
+import os
+
+import numpy as np
+import pytest
 import pyvista as pv
 import tetgen
-import numpy as np
-
-r = 0.1 # 100mm
-L = 0.5 # 500mm, distance of centers
-# result: 6.99pF in air
 
 
-airbox = pv.Cube(center=[0, 0, 0], x_length=3*L, y_length=L, z_length=L).triangulate()
-sphere1 = pv.Sphere(theta_resolution=16, phi_resolution=16, center=[-L/2,0,0], radius=r)
-sphere2 = pv.Sphere(theta_resolution=16, phi_resolution=16, center=[L/2,0,0], radius=r)
-mesh = pv.merge([sphere1,sphere2, airbox])
-
-plotter = pv.Plotter(shape=(1, 2))
-
-plotter.add_mesh(mesh, show_edges=True, opacity=0.5, label='Input Mesh')
-plotter.add_text("Input Mesh", font_size=10)
-plotter.subplot(0, 1)
-
-
-plotter.add_text("Tetrahedralized Mesh", font_size=10)
-
-tgen = tetgen.TetGen(mesh)
-
-V_sphere = 4/3 * np.pi * r**3
-
-tgen.addRegion(100, [-L/2, 0, 0.0], V_sphere / 1000)
-tgen.addRegion(200, [L/2, 0, 0.0], V_sphere / 1000)
-tgen.addRegion(300, [L*1.5, 0, 0.0], V_sphere / 100)
-
-nodes, elem, attrib = tgen.tetrahedralize(switches="pzq1.4Aa")
-
-grid = tgen.grid
-
-regions = np.unique(attrib[:, 0])
+def test_tetrahedralize_regions():
     
-for reg_id in regions:
-    name = f'Region {reg_id}'
-    reg_mask = attrib[:, 0] == reg_id
-    subgrid = grid.extract_cells(reg_mask)
-        
-    if reg_id == 100:
-        color = 'red'
-    elif reg_id == 200:
-        color = 'blue'
-    else:
-        color = 'white'
-        
-    if reg_id < 300:
-        opacity = 1.0
-    else:
-        opacity = 0.25
-        
-    plotter.add_mesh(subgrid, show_edges=True, color=color, opacity=opacity, label=name)
+    airbox = pv.Cube(center=[0, 0, 0], x_length=1.5, y_length=0.5, z_length=0.5).triangulate()   
+    sphere1 = pv.Sphere(theta_resolution=16, phi_resolution=16, center=[-0.25,0,0], radius=0.1)
+    sphere2 = pv.Sphere(theta_resolution=16, phi_resolution=16, center=[0.25,0,0], radius=0.1)
+    mesh = pv.merge([sphere1,sphere2, airbox])
 
-plotter.add_legend()
 
+    tgen = tetgen.TetGen(mesh)
     
-plotter.link_views()
-plotter.show()
+    V_sphere = 4/3 * 3.1415 * 0.1**3
+    tgen.addRegion(100, [-0.25, 0, 0], V_sphere / 5000)     # sphere 1 
+    tgen.addRegion(200, [0.25/2, 0, 0], V_sphere / 5000)    # sphere 2    
+    tgen.addRegion(300, [1.5, 0, 0], V_sphere / 100)        # airbox
+    
+    _, elem, attrib = tgen.tetrahedralize(switches="pzq1.4Aa")
 
-exit()
-
-
-grid.plot(show_edges=True, scalars=colors, opacity=0.5)
-
-# get cell centroids
-cells = grid.cells.reshape(-1, 5)[:, 1:]
-cell_center = grid.points[cells].mean(1)
-
-# extract cells below the 0 xy plane
-mask = cell_center[:, 2] < 0
-cell_ind = mask.nonzero()[0]
-subgrid = grid.extract_cells(cell_ind)
-
-cell_qual = subgrid.compute_cell_quality()['CellQuality']
-
-# advanced plotting
-plotter = pv.Plotter()
-plotter.add_mesh(subgrid, lighting=True, show_edges=True, scalars=cell_qual, cmap='bwr', clim=[0, 1], flip_scalars=True)
-
-plotter.add_mesh(mesh, 'r', 'wireframe')
-plotter.add_legend([[' Input Mesh ', 'r'],
-                    [' Tessellated Mesh ', 'black']])
-plotter.show()
-
-# subgrid.plot(scalars=cell_qual, cmap='bwr', clim=[0, 1], flip_scalars=True, show_edges=True)
-
-# slices = tgen.grid.slice_orthogonal()
-
-# slices.plot(show_edges=True)
+    assert attrib is not None
+    assert len(attrib) == len(elem)
+    
+    regions = np.unique(attrib[:, 0])
+    
+    assert len(regions) == 3
