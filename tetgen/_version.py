bf8c833e9846095acc451f5d58e946bd42e5d02d
Alex Kaszynski <akascap@gmail.com>
2025-07-02 14:37:17 -0600

    v0.6.6

--- tetgen/_version.py
+++ tetgen/_version.py
@@ -8,7 +8,7 @@ version_info = 0, 27, 'dev0'
 """
 
 # major, minor, patch, -extra
-version_info = 0, 6, 5
+version_info = 0, 6, 6
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
