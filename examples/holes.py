a1a1561680b4df18a2b39d76841769df4e0857e1
thefloe <tfloery@gmail.com>
2025-07-13 11:33:27 +0200

    corrected docstring in examples

--- examples/holes.py
+++ examples/holes.py
@@ -1,16 +1,20 @@
-# """
-# Holes
-# ~~~~~~~~~~~~
-# Example of using TetGen to create a tetrahedral mesh with holes.
-# """
+"""
+Holes
+~~~~~~~~~~~~
+
+Example of using TetGen to create a tetrahedral mesh with holes.
+
+"""
 
 import pyvista as pv
 import tetgen
 
+# create a cube with two spheres inside it
 cube = pv.Cube(x_length=2).triangulate()
 sphere1 = pv.Sphere(center=[-0.5, 0.0, 0.0], theta_resolution=16, phi_resolution=16, radius=0.25)
 sphere2 = pv.Sphere(center=[0.5, 0.0, 0.0], theta_resolution=16, phi_resolution=16, radius=0.1)
 
+# merge the geometries
 mesh = pv.merge([cube, sphere1, sphere2])
 
 tgen = tetgen.TetGen(mesh)
@@ -19,7 +23,9 @@ tgen = tetgen.TetGen(mesh)
 tgen.add_hole([-0.5, 0.0, 0.0])
 tgen.add_hole([0.5, 0.0, 0.0])
 
+# tetrahedralize the mesh using TetGen
 nodes, elem = tgen.tetrahedralize(switches="pzq1.4")
 grid = tgen.grid
 
+# plot a slice to see the holes
 grid.slice(normal="z").plot(show_edges=True, cpos="xy")
