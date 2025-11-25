c79f027643e817d0f2e81fe595ba3c0d76d1bdf5
Alex Kaszynski <akascap@gmail.com>
2025-11-25 13:26:57 -0700

    use dev version v0.8.dev0

--- tetgen/_version.py
+++ tetgen/_version.py
@@ -1,6 +1,6 @@
 """tetgen package version.
 
-On the ``master`` branch, use 'dev0' to denote a development version.
+On the ``main`` branch, use 'dev0' to denote a development version.
 For example:
 
 version_info = 0, 27, 'dev0'
@@ -8,7 +8,7 @@ version_info = 0, 27, 'dev0'
 """
 
 # major, minor, patch, -extra
-version_info = 0, 7, "dev0"
+version_info = 0, 8, "dev0"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
